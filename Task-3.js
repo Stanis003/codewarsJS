32941d8ae89269dcac6e176deb173df9ae5a4160
Stanis003 <41126587+Stanis003@users.noreply.github.com>
2020-11-20 22:21:11 +0200

    Update Task-3.js
    Edit

--- Task-3.js
+++ Task-3.js
@@ -10,52 +10,44 @@ class SmallestIntegerFinder {
 2) https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
   function circleCircumference(circle) {
-    return +(2*Math.PI*circle.radius).toFixed(6);
+    return 2*Math.PI*circle.radius;
     }
 
 3) https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
 
-class Shark extends Animal {
-    constructor(name, age, status) {
-      super(name, age, status);
-      this.legs=0;
-      this.species = "shark";
-      this.status=status;
-    }
-  }
-  
-  class Cat extends Animal {
-    constructor (name,age,status){
-      super(name, age, status);
-      this.legs=4;
-      this.species="cat";
-      this.status=status;
-       }
-     introduce(){
-         return `${super.introduce()}  Meow meow!`;
-       }
-  }
-  
-  class Dog extends Animal {
-    constructor(name, age, status, master){
-      super(name, age,status);
-      this.status=status;
-      this.master=master;
-      this.legs=4;
-      this.species = "dog";
+
+    class Shark extends Animal {
+        constructor(name, age, status) {
+          super(name, age, 0, "shark", status);
+          }
       }
-      greetMaster(){
-      return `Hello ${this.master}`;
+      
+      class Cat extends Animal {
+        constructor (name,age,status){
+          super(name, age, 4, "cat", status);
+           }
+         introduce(){
+             return `${super.introduce()}  Meow meow!`;
+           }
       }
-  }
+      
+      class Dog extends Animal {
+        constructor(name, age, status, master){
+          super(name, age, 4, "dog", status);
+          this.master=master;
+          }
+          greetMaster(){
+          return `Hello ${this.master}`;
+          }
+      }
 
 4) https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 
-function giveMeFive(obj){
-  let arr=[];
-  for (var key in obj){
-    arr.push(key);
-    arr.push(obj[key]);
-    }
-   return arr.filter(a=>{return a.length==5;});
-}
+    function giveMeFive(obj){
+        const arr=[];
+        for (let key in obj){
+          arr.push(key);
+          arr.push(obj[key]);
+          }
+         return arr.filter(el=> el.length===5);
+      }
