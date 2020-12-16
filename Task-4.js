62d5fcff56f3935bad26c9ff6598899cb8ff24a8
St. 1 <spirit9969@gmail.com>
2020-12-16 22:14:32 +0000

    add kata

--- Task-4.js
+++ Task-4.js
@@ -4,3 +4,9 @@ var gimme = function (inputArray) {
 let arr = [...inputArray].sort((a,b)=>a>b);
 return inputArray.findIndex(value=>value===arr[1]);
 } 
+
+
+//https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+function pigIt(str){
+  return str.split(' ').map(elem=>/[a-zA-Z]+/.test(elem)?elem.slice(1)+elem[0]+'ay':elem).join(' ');
+}
